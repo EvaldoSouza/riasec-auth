6a14625db9f8e4ce087c2aaf54a6d7505626b2a4
Evaldo Souza <evaldo.souza2@gmail.com>
2025-09-28 10:39:58 -0300

    Não funcionou as alterações. Tentando algo novo

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Tells Next.js that your application lives under the /riasec360 path
+  basePath: '/riasec360',
+
+  // Ensures that static assets (CSS, JS, images) also use this path
+  assetPrefix: '/riasec360',
   
 };
 
